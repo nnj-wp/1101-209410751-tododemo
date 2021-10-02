e3c6e3048628b58bc765a88e6a0bb55e39e4a8c7
nnj-wp <nnj448jack@gmail.com>
2021-10-02 22:39:04 +0800

    first commit

---
+++ src/App_51.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Todo_51 from './components/Todo_51';
+
+function App_51() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_51 text="Learn React" />
+      <Todo_51 text="Learn SQL+MarkDown" />
+      <Todo_51 text="study diary" />
+    </div>
+  );
+}
+
+export default App_51;
