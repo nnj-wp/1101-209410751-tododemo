f5645fec210d99c720e322b83735fa71a5ac2496
nnj-wp <nnj448jack@gmail.com>
2021-10-07 19:54:27 +0800

    第二次

---
+++ src/components/Backdrop_51.js
@@ -0,0 +1,5 @@
+function Backdrop_51(props) {
+  return <div className="backdrop" onClick={props.onClose}></div>;
+}
+
+export default Backdrop_51;
--- src/components/Todo_51.js
+++ src/components/Todo_51.js
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+import Backdrop_51 from './Backdrop_51';
+import Modal_51 from './Modal_51';
+
+
 function Todo_51(props) {
+  const [showModal, setShowModal] = useState();
+
+  function showModalHandler(){
+    setShowModal(true);
+  }
+
+  function closeModalHandler(){
+    setShowModal(false);
+  }
+
   return (
     <div>
       <div className="card">
         <h2>{props.text}</h2>
         <div className="actions">
-          <button className="btn">Delete</button>
+          <button className="btn" onClick = {showModalHandler}>Delete</button>
         </div>
       </div>
+      {showModal && <Backdrop_51 onClose= {closeModalHandler}/>}
+      {showModal && <Modal_51 text = 'Are yor sure ??' onClose= {closeModalHandler}/>}
     </div>
   );
 }
